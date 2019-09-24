61cf368eaded29c6429712249cf1acc474c83d51
Kevin Wallace <wallacekn@comcast.net>
2019-09-23 23:43:16 -0700

    Add run files

---
+++ sortphotos.sh
@@ -0,0 +1,2 @@
+/usr/local/sortphotos/bin/sortphotos -r -c --rename %Y_%m%d_%H%M /media/sf_shared/Pics.In /media/sf_shared/Pictures
+mv /media/sf_shared/Pics.In/* /media/sf_shared/backup
---
+++ sortvideos.sh
@@ -0,0 +1,2 @@
+/usr/local/sortphotos/bin/sortphotos -r -c --rename %Y_%m%d_%H%M /media/sf_shared/Videos.In /media/sf_shared/Videos
+mv /media/sf_shared/Videos.In/* /media/sf_shared/backup
